6cfc3a97b270db638c0c257f53d40023ec949a67
lucharrier <lucharrier@gmail.com>
2022-02-22 20:00:19 +0100

    Added git file

--- bin/install_calendso.sh
+++ bin/install_calendso.sh
@@ -9,7 +9,7 @@ function fetch_calendso_dist() {
     local location="$1"
     calendso_version="heads/master"
     calendso="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
-    curl -sL $calendso | \
-        tar zxvf - --strip-components 1 -C "$location"
+    curl -o "$location/dir" $calendso
+    tar xzf "$location/dir" -C "$location"
     echo "Calendso backend installed"
 }
