7bd6ae8ba44d04635aa920088f9c42c32302c2b5
lucharrier <lucharrier@gmail.com>
2022-02-22 18:16:31 +0100

    Fixed calendso dir

--- bin/install_calendso.sh
+++ bin/install_calendso.sh
@@ -10,10 +10,9 @@ echo "# $(basename $0)"
 calendso_version="heads/master"
 calendso="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
 calendso_dir="calendso"
-
 mkdir errbackends
 ( cd errbackends  ;
-  mkdir calendso_dir
+  mkdir $calendso_dir
   curl -sL $calendso | \
     tar zxvf - --strip-components 1 -C $calendso_dir
 )
