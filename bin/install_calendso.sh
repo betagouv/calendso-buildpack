a1de44ad511f576868b3b21f7d9c887d52e9318c
lucharrier <lucharrier@gmail.com>
2022-02-22 19:00:14 +0100

    Changed function to fetch calendso

--- bin/install_calendso.sh
+++ bin/install_calendso.sh
@@ -5,16 +5,13 @@
 
 set -euo pipefail
 
-echo "# $(basename $0)"
+function fetch_calendso_dist() {
+    local location="$1"
+    calendso_version="heads/master"
+    calendso="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
+    curl -sL $calendso | \
+        tar zxvf - --strip-components 1 -C "$location"
+    )
 
-calendso_version="heads/master"
-calendso="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
-calendso_dir="calendso"
-mkdir errbackends
-( cd errbackends  ;
-  mkdir $calendso_dir
-  curl -sL $calendso | \
-    tar zxvf - --strip-components 1 -C $calendso_dir
-)
-
-echo "Calendso backend installed"
+    echo "Calendso backend installed"
+}
