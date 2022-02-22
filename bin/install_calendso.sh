51bb0dffe0447c77cba64ee7e32dff5ce6bec88c
lucharrier <lucharrier@gmail.com>
2022-02-22 16:19:34 +0100

    Added calendso buildpack init files

---
+++ bin/install_calendso.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# install backends and storage
+#
+
+set -euo pipefail
+
+echo "# $(basename $0)"
+
+calendso_version="heads/master"
+calendso="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
+calendso_dir="calendso"
+
+mkdir errbackends
+( cd errbackends  ;
+  mkdir calendso_dir
+  curl -sL $calendso | \
+    tar zxvf - --strip-components 1 -C $calendso_dir
+)
+
+echo "Calendso backend installed"
