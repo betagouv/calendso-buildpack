1596f3916f4770ad8d652bd4f7b2a6a7f34f8699
lucharrier <lucharrier@gmail.com>
2022-02-23 09:39:47 +0100

    Added docker-compose for local test and fix compile

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calendso-buildpack
+
+docker run --name calendso -it -p 8065:8065 -v "$(pwd)"/.env:/env/.env -v "$(pwd)":/buildpack scalingo/scalingo-18:latest bash
--- bin/install_calendso.sh
+++ bin/install_calendso.sh
@@ -3,14 +3,18 @@
 # install backends and storage
 #
 
-set -euo pipefail
+CURL="curl -L --retry 15 --retry-delay 2" # retry for up to 30 seconds
 
 function fetch_calendso_dist() {
     local location="$1"
     calendso_version="heads/master"
-    calendso="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
-    curl -o "calendso.tar.gz" "${calendso}"
-    tar xzf "calendso.tar.gz" -C "$location"
-
     echo "Calendso backend installed"
+    local dist="calendso"
+    local dist_url="https://github.com/calcom/cal.com/archive/refs/${calendso_version}.tar.gz"
+    if [ -f "${CACHE_DIR}/dist/${dist}" ]; then
+        info "File is already downloaded"
+    else
+        ${CURL} -o "${CACHE_DIR}/dist/${dist}" "${dist_url}"
+    fi
+    tar xzf "$CACHE_DIR/dist/${dist}" -C "$location"
 }
