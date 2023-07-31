b7fb516fbc3f962d4aff5cb2b6c0ac9ad35d1fc4
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2023-07-31 16:03:11 +0200

    bin: better debug

--- bin/install_calendso.sh
+++ bin/install_calendso.sh
@@ -8,12 +8,12 @@ CURL="curl -L --retry 15 --retry-delay 2" # retry for up to 30 seconds
 function fetch_calendso_dist() {
     local location="$1"
     local version="$2"
-    echo "Calendso backend installed"
     local dist="calendso"
     local dist_url="https://github.com/calcom/cal.com/archive/refs/tags/${version}.tar.gz"
     if [ -f "${CACHE_DIR}/dist/${dist}" ]; then
         echo "File is already downloaded"
     else
+        echo "Downloading Calendso release: $dist_url"
         ${CURL} -o "${CACHE_DIR}/dist/${dist}" "${dist_url}"
     fi
     tar xzf "$CACHE_DIR/dist/${dist}" -C "$location"
